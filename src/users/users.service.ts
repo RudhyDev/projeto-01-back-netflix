19a493750a348864730ac3358554c6d7f84f9cef
Rudhy Pereira <rudhy.pereira@gmail.com>
2021-11-06 19:48:41 -0300

    ..

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma, User } from '@prisma/client'
+import { PrismaService } from 'src/prisma.service'; 
+import { UserRole } from './enum/role.enum';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(private db: PrismaService){}
+
+    async create(data: Prisma.UserCreateInput, role: UserRole){}
+
+}
