1aefd6ef26fb10c6d77880e49887e79716612727
Rudhy Pereira <rudhy.pereira@gmail.com>
2021-11-06 22:15:32 -0300

    rotas posts criadas

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,12 +2,35 @@ import { Injectable, ConflictException } from '@nestjs/common';
 import { Prisma, User } from '@prisma/client'
 import { PrismaService } from 'src/prisma.service'; 
 import { UserRole } from './enum/role.enum';
+import * as bcrypt from 'bcrypt';
+
 
 @Injectable()
 export class UsersService {
     constructor(private db: PrismaService){}
 
-    async create(data: Prisma.UserCreateInput, role: UserRole): Promise<User> {return}
+    async create(data: Prisma.UserCreateInput, role: UserRole): Promise<User> {
+        const userExists = await this.db.user.findUnique({
+            where: { email: data.email},
+        });
+
+        if(userExists){
+            throw new ConflictException('Esse e-mail já foi cadastrado!')
+        }
+
+        const hashedPassword = await bcrypt.hash(data.password, 10)
+
+        const user = await this.db.user.create({
+            data: {
+                ...data,
+                role: role,
+                password: hashedPassword
+            }
+        });
+
+        delete user.password;
+        return user;
+    }
     
 
 }
