d45086d87e77e62e5c681664b0a25bdacb8cfbf2
Rudhy Pereira <rudhy.pereira@gmail.com>
2021-11-09 21:42:53 -0300

    CRIAÇÃO DO GUARD

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,8 +1,10 @@
-import { Controller, Body, Post, Get, Param, UnprocessableEntityException, Delete } from '@nestjs/common';
-import { PrismaPromise, User } from '@prisma/client';
+import { Controller, Body, Post, Get, Param, UnprocessableEntityException, Delete, UseGuards } from '@nestjs/common';
+import { User } from '@prisma/client';
 import { CreateUserDto } from './DTO/create-user.dto';
 import { UsersService } from './users.service';
 import { UserRole } from './enum/role.enum';
+import { SimpleGuard } from 'src/auth/simple.guard';
+
 
 
 @Controller()
@@ -15,6 +17,7 @@ export class UsersController {
         return this.service.create(data, UserRole.USER)
     };
 
+    @UseGuards(SimpleGuard)
     @Post('create-admin')
     createAdmin(@Body() data: CreateUserDto): Promise<User>{
         delete data.passwordConfirmation;
